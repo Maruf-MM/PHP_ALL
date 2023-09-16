9f950ab5507a53c513f562845d48f6424df368ed
Maruf <maruf236hasan@gmail.com>
2023-09-16 16:51:07 +0600

    2

--- 04.PHP_OOP_Fundamentals_Tutorial/16.Autoloading_Classes.php
+++ 04.PHP_OOP_Fundamentals_Tutorial/16.Autoloading_Classes.php
@@ -3,7 +3,6 @@ include "header.php";
 // include dirname(__FILE__)."/Classes/16.1.Autoloading_Classes.php";
 spl_autoload_register(function($class_name)
 {
-  echo $class_name;
   include $class_name.".php";
 });
   $java = new java();
