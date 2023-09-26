d037879b8b22a69189f782ac0c9ec1c639a7cef5
Maruf <maruf236hasan@gmail.com>
2023-09-26 13:26:16 +0600

    k

--- 04.PHP_OOP_Fundamentals_Tutorial/22.Late_StaticBinding_in_PHP.php
+++ 04.PHP_OOP_Fundamentals_Tutorial/22.Late_StaticBinding_in_PHP.php
@@ -4,9 +4,36 @@ class php
 {
   public static function framework()
   {
-    echo "Cake PHP a framework";
-    echo "wow!";
+    echo "Cake PHP is a framework"."<br>";
+  }
+  public function getname()
+  {
+    static::framework();
+  }
+  public static function classname()
+  {
+    return __CLASS__."<br>";
   }
 }
+class childphp extends php
+{
+  public static function framework()
+  {
+    echo "Laravel is a framework"."<br>";
+  }
+  public static function classname()
+  {
+    return __CLASS__."<br>";
+  }
+}
+$obj= new php;
+$obj->getname();
+echo $obj->classname();
+
+$obj1= new childphp;
+$obj1-> getname();
+echo $obj1->classname();
+childphp::framework();
+
 include "footer.php";
 ?>
